5812ec7e8b4503b40b695586c0c2a3e96e8b8372
eunmin <telepopsound@gmail.com>
2016-07-03 15:17:29 +0900

    1장 예제 추가

---
+++ 1_hello_world/webapp/src/webapp/core.clj
@@ -0,0 +1,12 @@
+(ns webapp.core
+  (:require [webapp.server :refer [run-jetty]]))
+
+(defn handler [request]
+  {:status 201
+   :headers {"Content-Type" "text/html;charset=utf-8"}
+   :body (str
+           "<p>method: " (:method request) "</p>"
+           "<p>path: " (:path request) "</p>")})
+
+(defn -main [& args]
+  (run-jetty handler))
---
+++ 1_hello_world/webapp/src/webapp/server.clj
@@ -0,0 +1,33 @@
+(ns webapp.server
+  (:require [clojure.string :refer [lower-case]])
+  (:import [javax.servlet.http HttpServletRequest HttpServletResponse]
+           [org.eclipse.jetty.server Server ServerConnector]
+           [org.eclipse.jetty.server.handler AbstractHandler]))
+
+(defn- build-request-map [^HttpServletRequest request]
+  (let [query-string (.getQueryString request)]
+    {:method (keyword (lower-case (.getMethod request)))
+     :path (str (.getPathInfo request)
+                (when query-string
+                  (str "?" query-string)))}))
+
+(defn- new-handler [handler]
+  (proxy [AbstractHandler] []
+    (handle [target base-request request ^HttpServletResponse response]
+      (let [request-map (build-request-map request)
+            {:keys [status headers body]} (handler request-map)
+            content-type (get headers "Content-Type")]
+        (.setStatus response status)
+        (.setContentType response content-type)
+        (with-open [writer (.getWriter response)]
+          (.print writer body))))))
+
+(defn run-jetty [handler]
+  (let [^Server server (Server.)
+        ^ServerConnector connector (doto (ServerConnector. server)
+                                     (.setPort 8080))]
+    (doto server
+      (.addConnector connector)
+      (.setHandler (new-handler handler))
+      (.start)
+      (.join))))
